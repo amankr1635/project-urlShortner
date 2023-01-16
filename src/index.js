7144cece5dd0134f428209a453e6e1e8426fcd78
Sanhil143 <sanhilrai143@gmail.com>
2023-01-16 17:14:59 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+
+
+const app = express()
+app.use(express.json())
+
+mongoose.connect("")
+.then(() => console.log("My DB is coonected"))
+.catch((err)=> console.error(err))
+
+
+app.use("/", route)
+
+app.listen(3000, () => {
+      console.log("Express app running on port " + 3000);
+})
