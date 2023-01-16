955a2ebbb4aa80250031fe2dd9298c75e00167c1
amankr1635 <amankr.1635@gmail.com>
2023-01-16 17:50:04 +0530

    Project-03 started

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
-
+const router = require("./routes/routes")
 const app = express()
 app.use(express.json())
 
-mongoose.connect("")
+mongoose.set('strictQuery', true)
+mongoose.connect("mongodb+srv://Amankr:pwwELCe59UIUh9mj@cluster0.oxwexg5.mongodb.net/group15Database",{useNewUrlParser: true})
 .then(() => console.log("My DB is coonected"))
 .catch((err)=> console.error(err))
 
 
-app.use("/", route)
+app.use("/", router)
 
 app.listen(3000, () => {
       console.log("Express app running on port " + 3000);
