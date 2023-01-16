c7bd1950e12dffb771421c9458146d51922ab904
Sagar <sagarmaan2015@gmail.com>
2023-01-16 17:54:04 +0530

    controller APIs

--- src/contorller/controller.js
+++ src/contorller/controller.js
@@ -1,1 +1,33 @@
-const urlModel = require("../models/urlModels");
+const urlModel = require("../models/urlModels")
+
+
+//=============Create API==============//
+
+exports.createUrl = async(req,res)=>{
+    try{
+       let data = req.body
+
+       let createData = await urlModel.create(data)
+
+       return res.status().send({status:true,data:createData})
+    }catch(error){
+        return res.status(500).send({status:false,message:error.message})
+    }
+}
+
+
+//==================Get API================//
+
+exports.getUrl = async (req,res) =>{
+    try{
+        let data = req.params
+
+        let getData = await urlModel.find()
+        
+        return res.status().send({status:false,data:getData})
+
+    }catch(error){
+        return res.status(500).send({status:false , message:error.message})
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const router = require("./routes/routes")
 const app = express()
+
 app.use(express.json())
 
 mongoose.set('strictQuery', true)
