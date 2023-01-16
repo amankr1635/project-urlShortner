76912ce5cc90cfc6760e655fb4c92922055bc931
Sanhil143 <sanhilrai143@gmail.com>
2023-01-16 18:10:41 +0530

    add

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../contorller/controller");
+const controller = require("../controllers/controller");
 
 
 
