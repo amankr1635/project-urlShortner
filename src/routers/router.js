6ec75e6579eeb49cdb38d49cb7e50f83983ed245
Sanhil143 <sanhilrai143@gmail.com>
2023-01-16 23:48:03 +0530

    add

--- src/routers/router.js
+++ src/routers/router.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const controller = require("../controllers/controller");
 
 
+router.post('/url/shorten', controller.create)
+
+router.get('/:urlCode', controller.getUrl)
 
 
-
-
-router.all("/*" ,function(req,res){
-    res.status(400).send({status:false, message: "Invalid Http Request" })
+router.all("/*", (req, res) => {
+    res.status(400).send({ status: false, message: "Invalid Http Request" })
 })
 
 module.exports = router
