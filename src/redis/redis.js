d543151e39759552fe6928d48a0ffa8a3c6a5578
amankr1635 <amankr.1635@gmail.com>
2023-01-18 23:54:57 +0530

    Project-03 completed

---
+++ src/redis/redis.js
@@ -0,0 +1,16 @@
+const redis = require("redis")
+
+const redisClint = redis.createClient(
+    13032,
+    "redis-13032.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClint.auth("ImsB8tvefgIR7PZ8ajhZVxFNjbo6Bb4f", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClint.on("connect", async function () {
+    console.log("connected to redis");
+  });
+
+  module.exports = redisClint
