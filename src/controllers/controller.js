23bf150555091ab4f26c4a206650b576855c0950
Sagar <sagarmaan2015@gmail.com>
2023-01-19 11:52:06 +0530

    message

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -18,24 +18,32 @@ const create = async (req, res) => {
         .status(400)
         .send({ status: false, message: "Please enter body on body" });
     }
+
     if (typeof body.longUrl !== "string") {
       return res
         .status(400)
         .send({ status: false, message: "Please enter valid url in string" });
     }
+
     let longUrl = await GET_ASYNC(`${body.longUrl}`);
+
     let objectConversion = JSON.parse(longUrl);
+
     if (longUrl) {
-      return res.status(200).send({status: true ,data : objectConversion});
+      return res.status(200).send({status: true ,message : "This message is from cache and this url is already exist " , data : objectConversion});
     }
+
     let checkUrl = await axios
       .get(body.longUrl)
       .then(() => body.longUrl)
       .catch((err) => null);
+
     if (!checkUrl) {
       return res.status(404).send({ status: false, message: "Not found" });
     }
+
     let createUrl = shortId.generate().toLowerCase();
+
     let baseUrl = "http://localhost:3000/";
     body.shortUrl = baseUrl + createUrl;
     body.urlCode = createUrl;
@@ -43,6 +51,7 @@ const create = async (req, res) => {
     let checkData = await urlModel
       .findOne({ longUrl: body.longUrl })
       .select({ urlCode: 1, longUrl: 1, shortUrl: 1, _id: 0 });
+
     if (checkData) {
       return res
         .status(200)
@@ -50,8 +59,11 @@ const create = async (req, res) => {
     }
 
     let createData = await urlModel.create(body);
+
     let urls = {longUrl:createData.longUrl, urlCode: createData.urlCode, shortUrl: createData.shortUrl}
-    await SET_ASYNC(`${body.longUrl}`, 60*1440, JSON.stringify(urls));
+
+    await SET_ASYNC(`${body.longUrl}`, 60*1, JSON.stringify(urls));
+
     return res.status(201).send({
       status: true,
       data: {longUrl:urls.longUrl, urlCode: urls.urlCode, shortUrl: urls.shortUrl}
@@ -72,20 +84,26 @@ const getUrl = async (req, res) => {
         .status(400)
         .send({ status: false, message: "urlcode is not valid" });
     }
+
     let createdUrl = await GET_ASYNC(`${param}`);
+
     let objectConversion = JSON.parse(createdUrl);
+
     if (createdUrl) {
       return res.status(302).redirect(objectConversion);
     } else {
       let getUrl = await urlModel
         .findOne({ urlCode: param })
         .select({ longUrl: 1, _id: 0 });
+
       if (!getUrl) {
         return res
           .status(404)
           .send({ status: false, message: "no such Url exist" });
       }
+
       let longUrl = getUrl.longUrl;
+
       await SET_ASYNC(`${param}`, 60 * 1440, JSON.stringify(longUrl));
       res.status(302).redirect(longUrl);
     }
