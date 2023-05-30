70e650974209164e2f9cfffe6191d1db43ab3699
amankr1635 <amankr.1635@gmail.com>
2023-05-30 13:08:12 +0530

    redis changed

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -1,18 +1,23 @@
 const redis = require("redis");
 require('dotenv').config();
 
-const redisClint = redis.createClient({
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOST,
-  no_ready_check: true,
-});
-redisClint.auth(process.env.REDIS_PASSWORD, function (err) {
-  if (err) throw err;
-});
+
+const client = redis.createClient(process.env.REDIS_URL);
+client.on('error', (err) => console.log('Redis Client Error', err))
+console.log("Connected to Redis..")
+
+// const redisClint = redis.createClient({
+//   port: process.env.REDIS_PORT,
+//   host: process.env.REDIS_HOST,
+//   no_ready_check: true,
+// });
+// redisClint.auth(process.env.REDIS_PASSWORD, function (err) {
+//   if (err) throw err;
+// });
 
 
-redisClint.on("connect", async function () {
-  console.log("connected to redis");
-});
+// redisClint.on("connect", async function () {
+//   console.log("connected to redis");
+// });
 
-module.exports = redisClint
+module.exports = client
--- src/routers/router.js
+++ src/routers/router.js
@@ -7,8 +7,6 @@ router.post('/url/shorten', controller.create)
 
 router.get('/:urlCode', controller.getUrl)
 
-console.log("i m here")
-
 router.all("/*", (req, res) => {
     res.status(400).send({ status: false, message: "invalid HTTP request" })
 })
