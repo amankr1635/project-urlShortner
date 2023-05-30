b2b9e585904d051dbe44806d6d61dbf8b85d83af
amankr1635 <amankr.1635@gmail.com>
2023-05-30 13:09:51 +0530

    removed line

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -6,18 +6,4 @@ const client = redis.createClient(process.env.REDIS_URL);
 client.on('error', (err) => console.log('Redis Client Error', err))
 console.log("Connected to Redis..")
 
-// const redisClint = redis.createClient({
-//   port: process.env.REDIS_PORT,
-//   host: process.env.REDIS_HOST,
-//   no_ready_check: true,
-// });
-// redisClint.auth(process.env.REDIS_PASSWORD, function (err) {
-//   if (err) throw err;
-// });
-
-
-// redisClint.on("connect", async function () {
-//   console.log("connected to redis");
-// });
-
 module.exports = client
